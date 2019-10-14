9f33459a52662f250f1f12fcaba757d1a7378d46
b7080037 <i.koryciarz@newcastle.ac.uk>
2019-10-14 11:18:22 +0100

    cli.py file created with cli() function

--- uniplot.py
+++ uniplot.py
@@ -1,11 +1,4 @@
-import gzip
-for l in gzip.open("uniprot_receptor (1).xml.gz"):
-    print(l.decode().strip())
 
-from Bio import SeqIO
-handle = gzip.open("uniprot_receptor (1).xml.gz")
-for record in SeqIO.parse(handle, "uniprot-xml"):
-    print(record)
 
 
 
