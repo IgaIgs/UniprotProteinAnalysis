89560cf5ddd4c6db75c19766254db827a290220c
Kirai <i.koryciarz@ncl.ac.uk>
2019-10-22 19:30:09 +0100

    Enabled the configuration of the Uniprot file location.

--- uniplot/cli.py
+++ uniplot/cli.py
@@ -61,23 +61,31 @@ def plot_average_by_taxa(args):
 
 
 def cli():
-    """Create a new parser and a list of subparsers."""
-
+    """Enable and simplify app management via the terminal."""
+    # Create the main parser.
     parser = argparse.ArgumentParser(prog='uniplot', description='Access some information about the proteins.')
+    # Add a second parser to enable file location configuration.
+    second_parser = argparse.ArgumentParser(prog='second_uniplot')
 
     subparsers = parser.add_subparsers(help='Sub Command Help')
 
     # Add subparsers
-    subparsers.add_parser('dump', help='Use dump function to display the whole file').set_defaults(func=dump,)
-    subparsers.add_parser('list', help='The list function displays a list of all protein names in the file').\
+    subparsers.add_parser('dump', help='Use dump function to display the whole file').set_defaults(func=dump, )
+    subparsers.add_parser('list', help='The list function displays a list of all protein names in the file'). \
         set_defaults(func=name)
-    subparsers.add_parser('average', help='This shows the average sequence length of the proteins in the file').\
+    subparsers.add_parser('average', help='This shows the average sequence length of the proteins in the file'). \
         set_defaults(func=average)
-    subparsers.add_parser('taxa_average', help='Display a bar graph of average protein sequence lengths by taxa').\
+    subparsers.add_parser('taxa_average_graph', help='Display a bar graph of average protein sequence lengths by taxa'). \
         set_defaults(func=plot_average_by_taxa)
 
+    # Add argument to the main parser so that the help message can be displayed.
+    parser.add_argument('--file_location', help="Input the location of the file after '--'")
+    # Add argument to the second parser to set a default file location.
+    second_parser.add_argument('--file_location', default='uniprot_receptor.xml.gz')
+
     # Parse the command line
-    args = parser.parse_args()
+    # Unknown to let the function accept new file_location and 'known' to use only arguments within the parser
+    args, unknown = parser.parse_known_args()
 
     # Take the func argument, which points to our function and call it
     args.func(args)
